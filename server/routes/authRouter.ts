3b284a507aef83a0ecb6127df72397e804b538fd
mohamad <msiysinyuy@gmail.com>
2022-01-28 11:43:57 +0100

    the login route done

--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.post('/register',validRegister, authCtrl.register)
 router.post('/active', authCtrl.activeAccount)
+router.post('/login', authCtrl.login)
 
 export default router;
