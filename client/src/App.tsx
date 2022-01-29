0fc83a28869f2eaab0c533eb6846e77f2405fe1e
mohamad <msiysinyuy@gmail.com>
2022-01-29 15:04:33 +0100

    the footer made

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import PageRender from './PageRender'
-import {Header} from './components'
+import {Header,Footer} from './components'
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
           <Route exact path="/:page" component={PageRender} />
           <Route exact path="/:page/:slug" component={PageRender} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
---
+++ client/src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+
+import './Footer.css'
+
+const Footer = ()=>{
+  return(
+    <div className="info__footer_container text-center bg-light py-4">
+      <h6>Welcome to the Information Center</h6>
+
+      <a href="https://msb-geneasocial.herokuapp.com/porfolio/home" target="_blank" rel="noreferrer" className="mb-2 d-block" >
+       Mohamad Siysinyuy
+      </a>
+      <p> Copyright &copy; 2022  </p>
+    </div>
+  )
+}
+
+export default Footer
