e70d71c6bbe43df7c33666c2e03242ad8e29fc8d
mohamad <msiysinyuy@gmail.com>
2022-01-29 11:00:15 +0100

    intial setup of redux

---
+++ client/src/components/notFound/NotFound.tsx
@@ -0,0 +1,11 @@
+
+import './NotFound.css'
+const NotFound = () =>{
+  return(
+    <div className=" info__notfound_container">
+      <h2 className="info__notfound_title"> 404 | NotFound </h2>
+    </div>
+  )
+}
+
+export default NotFound
