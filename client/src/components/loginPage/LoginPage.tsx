4659678c86a742c4b60b8c47c7d13173326355d6
mohamad <msiysinyuy@gmail.com>
2022-02-01 10:14:33 +0100

    the login page initial commit

--- client/src/components/loginPage/LoginPage.tsx
+++ client/src/components/loginPage/LoginPage.tsx
@@ -1,9 +1,11 @@
-
+import './LoginPage.css'
 
 const LoginPage = () =>{
   return(
-    <div>
-      <h2>login</h2>
+    <div className="info__auth_page">
+      <div className="info__auth_auth">
+        <h3 className="info__auth_title">login</h3>
+      </div>
     </div>
   )
 }
