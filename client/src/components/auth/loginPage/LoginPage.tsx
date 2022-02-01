c17a976159051d5a2ba05109ecd89f8f528d99f8
mohamad <msiysinyuy@gmail.com>
2022-02-01 13:39:19 +0100

    the login page updated

--- client/src/components/auth/loginPage/LoginPage.tsx
+++ client/src/components/auth/loginPage/LoginPage.tsx
@@ -1,13 +1,33 @@
 import './LoginPage.css'
 import {LoginPass} from "../../../components"
+import {Link} from 'react-router-dom'
+import {useState} from 'react'
 
 
 const LoginPage = () =>{
+  const [sms, setSms] = useState(false)
   return(
     <div className="info__auth_page">
       <div className="info__auth_auth">
         <h3 className="info__auth_title">login</h3>
         <LoginPass />
+
+        <small className="info__forgot_password_link_container">
+            <span className="info__forgot_password_link ">
+              <Link to="/forgot_password" > Forgot password</Link>
+            </span>
+
+            <span className="info__forgot_password_link" onClick={() =>setSms(!sms)}>
+            {sms? 'Sign in with password' : 'Sign in with SMS'}
+            </span>
+        </small>
+
+        <p>
+          You don't have an account?
+          <Link to={`/register`} className="info__register_link">Register Now</Link>
+        </p>
+
+
       </div>
     </div>
   )
