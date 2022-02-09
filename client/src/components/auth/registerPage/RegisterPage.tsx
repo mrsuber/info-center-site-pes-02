5c3c70088401a637efb4e6223f970f8b2d979d02
mohamad <msiysinyuy@gmail.com>
2022-02-09 13:47:17 +0100

    the front-end registration done

--- client/src/components/auth/registerPage/RegisterPage.tsx
+++ client/src/components/auth/registerPage/RegisterPage.tsx
@@ -1,5 +1,5 @@
 import "./RegisterPage.css"
-import {LoginPass,LoginSMS} from "../../../components"
+import {RegisterForm} from "../../../components"
 import {Link} from 'react-router-dom'
 import {useState} from 'react'
 
@@ -9,22 +9,11 @@ const RegisterPage = () =>{
   return(
     <div className="info__auth_page">
       <div className="info__auth_box">
-        <h3 className="info__auth_title">Login to info Center</h3>
-        {sms ? <LoginSMS/> : <LoginPass /> }
-
-        <small className="info__forgot_password_link_container">
-            <span className="info__forgot_password_link ">
-              <Link to="/forgot_password" > Forgot password</Link>
-            </span>
-
-            <span className="info__forgot_password_link2" onClick={() =>setSms(!sms)}>
-            {sms? 'Sign in with password' : 'Sign in with SMS'}
-            </span>
-        </small>
-
+        <h3 className="info__auth_title">Register to info Center</h3>
+        <RegisterForm />
         <p>
-          {`You don't have an account? `}
-          <Link to={`/register`} className="info__register_link"> Register Now</Link>
+          {`Already have an account? `}
+          <Link to={`/login`} className="info__register_link"> Login Now</Link>
         </p>
 
 
